ca431928d9b8fb7ea5a572c8bd5029568dc2114a
nikiangel099 <138067509+nikiangel099@users.noreply.github.com>
2023-07-18 15:58:16 -0400

    Update irradiation_run_analysis.py

--- irradiation_run_analysis.py
+++ irradiation_run_analysis.py
@@ -1,5 +1,3 @@
-# import pyvisa
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -11,28 +9,26 @@ plt.xlabel('time (s)')
 plt.ylabel('Voltage (mV)')
 plt.rcParams["figure.figsize"] = [20, 15]
 
-if read_irr_run_file:
-    generator = read_irr_file()
-    time_pre = int(next(generator))
-    time_dis = int(next(generator))
-    time_post = int(next(generator))
-    total_time = time_pre + time_dis + time_post
-    T_cal= next(generator)
-    R_burster= next(generator)
-    tme = next(generator)
-    voltages = next(generator)
-    plt.plot(tme, voltages, c = 'red')
-    plt.show()
+
+generator = read_irr_file() # Variable are read from read_from_file.py
+time_pre = int(next(generator))
+time_dis = int(next(generator))
+time_post = int(next(generator))
+total_time = time_pre + time_dis + time_post
+T_cal= next(generator)
+R_burster= next(generator)
+tme = next(generator)
+voltages = next(generator)
+plt.plot(tme, voltages, c = 'red')
+plt.show()
 
 
-plt.plot(tme[1:time_pre - 4], voltages[1:time_pre - 4], c='blue')    
-# plt.scatter(total_time -time_dis-time_post + 0.5*time_dis, pre_volt)
-plt.plot(tme[time_pre + time_dis + 5:total_time + 1],voltages[time_pre + time_dis + 5:total_time + 1], c='red')
-# plt.scatter(total_time -time_dis-time_post + 0.5*time_dis, post_volt)
+plt.plot(tme[1:time_pre - 4], voltages[1:time_pre - 4], c='blue') # pre-drift is plotted in blue
+plt.plot(tme[time_pre + time_dis + 5:total_time + 1],voltages[time_pre + time_dis + 5:total_time + 1], c='red') # pre-drift is plotted in red
 
 def ohm_calibration(time_pre_ohm, time_ohm, time_post_ohm, total_time_ohm, tme_ohm, voltages_ohm):
-    a, b = np.polyfit(tme_ohm[1:time_pre_ohm - 4],voltages_ohm[1:time_pre_ohm - 4], 1)
-    pre_volt = a * (total_time_ohm - time_ohm - time_post_ohm + 0.5 * time_ohm) + b
+    a, b = np.polyfit(tme_ohm[1:time_pre_ohm - 4],voltages_ohm[1:time_pre_ohm - 4], 1) # Line of best fit of pre-drift
+    pre_volt = a * (total_time_ohm - time_ohm - time_post_ohm + 0.5 * time_ohm) + b # Forward extrapolated to determine value at midpoint
   
     a, b = np.polyfit(tme_ohm[time_pre_ohm + 5 :total_time_ohm - time_post_ohm - 4],voltages_ohm[time_pre_ohm + 5:total_time_ohm - time_post_ohm - 4], 1)
     top_volt = a * (total_time_ohm - time_ohm - time_post_ohm + 0.5 * time_ohm) + b
@@ -40,28 +36,26 @@ def ohm_calibration(time_pre_ohm, time_ohm, time_post_ohm, total_time_ohm, tme_o
     a, b = np.polyfit(tme_ohm[time_pre_ohm + time_ohm + 5:total_time_ohm + 1],voltages_ohm[time_pre_ohm + time_ohm + 5:total_time_ohm + 1], 1)
     post_volt = a * (total_time_ohm - time_ohm - time_post_ohm + 0.5 * time_ohm) + b
 
-    calibration = (((top_volt - pre_volt) + (top_volt - post_volt))/2)*1e-3
+    calibration = (((top_volt - pre_volt) + (top_volt - post_volt))/2)*1e-3 # The average between the two delta_V
     
-    return calibration
+    return calibration # returns (deltaV/deltaR)_1ohm
 
 def deltaV_to_deltaT(time_pre, time_dis, time_post, total_time, tme, voltages, T_cal, R_burster):
     a, b = np.polyfit(tme[1:time_pre - 4],voltages[1:time_pre - 4], 1)
-    pre_volt = a * (total_time - time_dis - time_post + 0.5 * time_dis) + b
-    
+    pre_volt = a * (total_time - time_dis - time_post + 0.5 * time_dis) + b 
        
     a, b = np.polyfit(tme[time_pre + time_dis + 5:total_time +1],voltages[time_pre + time_dis + 5:total_time +1], 1)
     post_volt = a * (total_time - time_dis - time_post + 0.5 * time_dis) + b
     
-    delta_volt = (post_volt - pre_volt)*1e-3
+    delta_volt = (post_volt - pre_volt)*1e-3 # deltaV (V)
 
      
-    #inst = access_multimeter()
+    #inst = access_multimeter() # # Communication with multimeter to read four series resistance for use in T_cal calculation function is imported from functions.py
     
     #inst.write("ROUTe:CLOSe (@1)")
     #inst.write("ROUTe:OPEN (@1)")
     # R_temp = float(inst.query(':MEASure:FRESistance?'))
-    R_temp = 114
-    temp_probe = 1
+    temp_probe = 1 # The below constants are based on which RTD probe is used
     
     if int(temp_probe) == 1:
         AI = 0.003942247616179
@@ -84,9 +78,7 @@ def deltaV_to_deltaT(time_pre, time_dis, time_post, total_time, tme, voltages, T
         BI = 2.09532e-6
         RI = 100.04198
     
-    # T_cal = (-1*AI+(math.sqrt((AI*AI)-(4*BI*(1-(R_temp/RI))))))/(2*BI)
-    
-    
+    T_cal = (-1*AI+(math.sqrt((AI*AI)-(4*BI*(1-(R_temp/RI))))))/(2*BI)
     beta = 3112.621146
     deltaV_deltaR = ohm_calibration(time_pre_ohm, time_ohm, time_post_ohm, total_time_ohm, tme_ohm, voltages_ohm)
     #deltaV_deltaR = 25e-6
